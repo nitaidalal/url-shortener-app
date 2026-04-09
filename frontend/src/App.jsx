119c596bf94a5864511c083b4e49fc218a9b850d
nitaidalal <dalalnitai7@gmail.com>
2026-04-10 02:27:53 +0530

    feat: implement routing and protected routes for user authentication and navigation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,64 @@
+import { Toaster } from 'react-hot-toast';
+import {Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import About from './pages/About';
+import SiteLayout from './components/SiteLayout';
+import { authService } from './services/api';
+import Profile from './pages/Profile';
+import Shortner from './pages/Shortner';
+
+
+function ProtectedRoute({ children }) {
+  const isAuthenticated = authService.isAuthenticated();
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
+}
+
+export default function App() {
+  return (
+    <>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            background: '#16161f',
+            color: '#e8e8f0',
+            border: '1px solid #252535',
+            borderRadius: '12px',
+            fontSize: '13px',
+          },
+        }}
+      />
+
+      <Routes>
+        <Route element={<SiteLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/shortener"
+            element={
+              <ProtectedRoute>
+                <Shortner />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+           />
+        </Route>
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+      </Routes>
+    </>
+  );
+}
