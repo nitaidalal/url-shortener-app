65cf54d809862803b408ff6ae5598a96ab3fc6c3
nitaidalal <dalalnitai7@gmail.com>
2026-04-10 02:19:52 +0530

    Initialize server with Express, middleware, and routing setup

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import urlRoutes from './routes/url.routes.js';
+import { redirectToOriginal } from './controllers/url.controller.js';
+import authRoutes from './routes/auth.routes.js';
+import connectDB from './utils/db.js';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+// ============== Built-in MIDDLEWARE ==============
+app.use(cookieParser());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
+app.use(express.json());
+
+// ============== ROUTES ==============
+app.use('/api/urls', urlRoutes);
+app.use("/api/auth", authRoutes);
+
+// Redirect route (We have to keep this at last because of the dynamic :code param)
+app.get('/:code', redirectToOriginal);
+
+
+app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}🚀`);
+    connectDB();
+});
